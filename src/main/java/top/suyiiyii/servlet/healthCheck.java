d3147fa9893e06f203efb45107a99cb3dde76059
suyiiyii <suyiiyii@gmail.com>
2024-04-21 17:15:10 +0800

    feat(health): 健康检查接口返回平台信息

--- src/main/java/top/suyiiyii/servlet/healthCheck.java
+++ src/main/java/top/suyiiyii/servlet/healthCheck.java
@@ -2,22 +2,36 @@ package top.suyiiyii.servlet;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.Data;
 import top.suyiiyii.su.WebUtils;
 import top.suyiiyii.su.servlet.BaseHttpServlet;
 
 import java.io.IOException;
 
 @WebServlet("/health")
-public class healthCheck extends BaseHttpServlet {
+public class healthCheck extends HttpServlet {
 
-    public healthCheck() {
-        super();
-    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        WebUtils.respWrite(resp, "healthy from " + System.getenv("HOSTNAME"));
+        HealthCheckResponse response = new HealthCheckResponse();
+        response.setPlatform(System.getenv("PLATFORM_NAME"));
+        response.setServer(System.getenv("HOSTNAME"));
+        if (response.getPlatform() == null) {
+            response.setPlatform("dangpay local");
+        }
+        if (response.getServer() == null) {
+            response.setServer("signleton");
+        }
+        WebUtils.respWrite(resp, response);
+    }
+
+    @Data
+    public static class HealthCheckResponse {
+        String platform;
+        String server;
     }
 }

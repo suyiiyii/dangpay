d5ee64ebb428af8a9bfa3cd1e34b949b5c2fd8d1
suyiiyii <suyiiyii@gmail.com>
2024-04-27 00:46:53 +0800

    feat(event): 添加权限验证

--- src/main/java/top/suyiiyii/service/EventService.java
+++ src/main/java/top/suyiiyii/service/EventService.java
@@ -1,9 +1,11 @@
 package top.suyiiyii.service;
 
 import top.suyiiyii.models.Event;
+import top.suyiiyii.su.IOC.Proxy;
 
 import java.util.List;
 
+@Proxy
 public interface EventService {
     void addEvent(Event event);
 

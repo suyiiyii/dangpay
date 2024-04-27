bf3b3be222129b813c50e004f6e2182efe902c28
suyiiyii <suyiiyii@gmail.com>
2024-04-27 23:41:39 +0800

    fix(mail): 发送邮箱验证码需要验证人机验证码

--- src/main/java/top/suyiiyii/servlet/user/RequireCode.java
+++ src/main/java/top/suyiiyii/servlet/user/RequireCode.java
@@ -3,19 +3,24 @@ package top.suyiiyii.servlet.user;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Data;
+import top.suyiiyii.service.CaptchaService;
 import top.suyiiyii.service.MailService;
 import top.suyiiyii.su.WebUtils;
 import top.suyiiyii.su.validator.Regex;
 
 public class RequireCode {
     private final MailService mailService;
+    private final CaptchaService captchaService;
 
-    public RequireCode(MailService mailService) {
+    public RequireCode(MailService mailService,
+                       CaptchaService captchaService) {
         this.mailService = mailService;
+        this.captchaService = captchaService;
     }
 
     public boolean doPost(HttpServletRequest req, HttpServletResponse resp) {
         RequireCodeReqeust request = WebUtils.readRequestBody2Obj(req, RequireCodeReqeust.class);
+        captchaService.verifyCaptcha(request.captcha);
         mailService.sendVerifyMail(request.email);
         return true;
     }
@@ -25,5 +30,6 @@ public class RequireCode {
     public static class RequireCodeReqeust {
         @Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
         public String email;
+        public String captcha;
     }
 }

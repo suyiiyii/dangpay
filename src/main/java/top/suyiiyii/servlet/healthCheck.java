5609859490726200251063cb3facbeb2e4beef5d
suyiiyii <suyiiyii@gmail.com>
2024-04-28 22:02:28 +0800

    feat(health): 添加日志

--- src/main/java/top/suyiiyii/servlet/healthCheck.java
+++ src/main/java/top/suyiiyii/servlet/healthCheck.java
@@ -6,12 +6,15 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import top.suyiiyii.su.IOC.IOCManager;
 import top.suyiiyii.su.WebUtils;
+import top.suyiiyii.su.exception.Http_500_InternalServerErrorException;
 import top.suyiiyii.su.orm.core.ModelManger;
 
 import java.io.IOException;
 
+@Slf4j
 @WebServlet("/health")
 public class healthCheck extends HttpServlet {
 
@@ -32,8 +35,11 @@ public class healthCheck extends HttpServlet {
         int connectionCount = IOCManager.getBean(ModelManger.class).getConnectionManger().getLeaveCapacity();
         if (connectionCount < 20) {
             resp.setStatus(500);
-            WebUtils.respWrite(resp, "数据库连接池不足");
+            log.error("数据库连接池不足");
+            throw new Http_500_InternalServerErrorException("数据库连接池不足");
         }
+        log.info("健康检查正常返回，数据库连接池剩余连接数：" + connectionCount);
+
     }
 
     @Data
